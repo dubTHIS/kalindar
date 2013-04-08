1235d7083f5b9d66e57f4d0576691bee5dc6990e
Jacob Wilhelm <jacobwilhelm20@gmail.com>
2013-04-07 20:30:23 -0400

    search for course and enrolled ins works

--- app/controllers/enrolled_ins_controller.rb
+++ app/controllers/enrolled_ins_controller.rb
@@ -1,19 +1,19 @@
 class EnrolledInsController < ApplicationController
   def new
-  	@course = Course.find(params[:id])
+  	@course = Course.find(params[:course])
   	@user = current_user
-  	@enrolled_in = EnrolledIn.build()
+  	@enrolled_in = EnrolledIn.new()
   	@enrolled_in.user_id = @user.id
   	@enrolled_in.course_id = @course.id
 
   	respond_to do |format|
-  		format.html { redirect_to courses_path}
+  		format.html { redirect_to courses_path, notice: 'Successfully added course.'}
   		format.json
 	end
   end
 
   def create
-  	@course = Course.find(params[:id])
+  	@course = Course.find(params[:course])
   	@user = current_user
   	@enrolled_in = EnrolledIn.build()
   	@enrolled_in.user_id = @user.id

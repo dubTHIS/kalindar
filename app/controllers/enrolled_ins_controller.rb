faac2018c9edcc9f1d82ac683f86ff18b37eb71e
Jacob Wilhelm <jacobwilhelm20@gmail.com>
2013-04-07 20:49:18 -0400

    enrolledins

--- app/controllers/enrolled_ins_controller.rb
+++ app/controllers/enrolled_ins_controller.rb
@@ -5,6 +5,9 @@ class EnrolledInsController < ApplicationController
   	@enrolled_in = EnrolledIn.new()
   	@enrolled_in.user_id = @user.id
   	@enrolled_in.course_id = @course.id
+  	@course.enrolled_ins << @enrolled_in
+  	@user.enrolled_ins << @enrolled_in
+  	@enrolled_in.save
 
   	respond_to do |format|
   		format.html { redirect_to courses_path, notice: 'Successfully added course.'}
@@ -15,9 +18,12 @@ class EnrolledInsController < ApplicationController
   def create
   	@course = Course.find(params[:course])
   	@user = current_user
-  	@enrolled_in = EnrolledIn.build()
+  	@enrolled_in = EnrolledIn.new()
   	@enrolled_in.user_id = @user.id
   	@enrolled_in.course_id = @course.id
+  	@course.enrolled_ins = @enrolled_in
+  	@user.enrolled_ins = @enrolled_in
+  	@enrolled_in.save
 
   	respond_to do |format|
   		format.html { redirect_to courses_path}
